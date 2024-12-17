f0d3861ef9004469b760b1751f1e49d4dd948f9c
Essamaki <contact.essamakiasmae@gmail.com>
2024-12-17 16:36:01 +0100

    frontend vercel url

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(i18nextMiddleware.handle(i18next))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://brikool-client.vercel.app',
     credentials: true,
 }));
 app.use(
