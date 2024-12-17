fa266497c94338c7517b267ce36627e916eef758
Essamaki <contact.essamakiasmae@gmail.com>
2024-12-17 16:47:01 +0100

    frontend vercel url

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const connectDB = require("./config/dbConnection");
 const i18next = require("i18next");
 const i18nextBackend = require("i18next-fs-backend");
 const i18nextMiddleware = require("i18next-http-middleware");
+const path = require("path");
 
 
 require('dotenv').config();
