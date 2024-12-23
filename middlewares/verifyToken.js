e86cb6ed16bf7d23f8b163b17a071f1fcb708e06
aymane <oujdimraymane@gmail.com>
2024-12-23 14:11:38 +0100

    log token

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -4,6 +4,7 @@ const User = require("../models/User");
 const verifyToken = (req, res, next) => {
     try {
         const token = req.cookies.jwt || req.headers['authorization']?.split(' ')[1];
+        console.log(token)
         if (!token) {
             return res.status(401).json({ message: "Unauthorized" });
         }
