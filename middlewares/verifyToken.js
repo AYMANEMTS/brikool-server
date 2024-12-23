4f6a7a4c4f06cf28715a2d7f76fae03a3e718a4d
aymane <oujdimraymane@gmail.com>
2024-12-23 14:25:02 +0100

    log token 2

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -4,29 +4,28 @@ const User = require("../models/User");
 const verifyToken = (req, res, next) => {
     try {
         const token = req.cookies.jwt || req.headers['authorization']?.split(' ')[1];
-        res.status(200).json(req.cookies.jwt)
-        // if (!token) {
-        //     return res.status(401).json({ message: "Unauthorized" });
-        // }
-        // verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
-        //     if (err) {
-        //         return res.status(403).json({ message: "Token expired or invalid" });
-        //     }
-        //     try {
-        //         const user = await User.findById(decoded.userId).select('-password');
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
+            if (err) {
+                return res.status(403).json({ message: "Token expired or invalid" });
+            }
+            try {
+                const user = await User.findById(decoded.userId).select('-password');
 
-        //         if (!user) {
-        //             return res.status(404).json({ message: "User not found or invalid token" });
-        //         }
-        //         req.userId = user._id
-        //         next()
-        //     } catch (error) {
+                if (!user) {
+                    return res.status(404).json({ message: "User not found or invalid token" });
+                }
+                req.userId = user._id
+                next()
+            } catch (error) {
 
-        //         return res.status(500).json({ message: "Error fetching user", error });
-        //     }
-        // });
+                return res.status(500).json({ message: "Error fetching user", error });
+            }
+        });
     }catch (e) {
-        return res.status(401).json({ success: false, message: "Unauthorized" });
+        return res.status(401).json({ success: false, message: "Unauthorized","req":req.cookies });
     }
 }
 
